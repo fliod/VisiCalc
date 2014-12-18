1668bbba978eaf2519018db081a197afca4f03f5
fliod <kgraceaz@gmail.com>
2014-12-18 15:42:49 -0700

    Update VisiCalc.java

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -1,17 +1,20 @@
 public class VisiCalc {
 
-	public static void main(String[] args) {
-		// create an array of cells
+ public static void main(String[] args) {
+  // create an array of cells
+   Cell[] cells=new Cell[12];
+  
+  // initialize the array
+   for(int i=0;i<cells.length;i++)
+   {
+     cells[i]=new CellString("");
+   }
+  // add test values
+   cells[0]=new CellString();
+   cells[1]=new CellString("wow");
+   cells[2]=new CellString();
+  
+  // print the array
 
-		
-		// initialize the array
-
-
-		
-		// add test values
-
-		
-		// print the array
-
-	}
+ }
 }
