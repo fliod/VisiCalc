c2b2888f60e35944d9968fa4135c4c383ba0aad8
fliod <kgraceaz@gmail.com>
2014-12-18 15:42:11 -0700

    Update CellNumeric.java

--- src/CellNumeric.java
+++ src/CellNumeric.java
@@ -1,1 +1,22 @@
 // placeholder
+public class CellNumeric extends Cell
+{
+  public CellNumeric()
+  {
+    super.setValue("");
+  }
+  public CellNumeric(double in)
+  {
+    String out=Double.toString(in);
+    super.setValue(out);
+  }
+  public double value()
+  {
+    return Double.parseDouble(super.getValue());
+  }
+  public void setValue(double in)
+  {
+    super.setValue(Double.toString(in));
+  }
+}
+  
