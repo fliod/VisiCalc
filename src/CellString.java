a563870db3eae80d7b3674a49170c28183d5514c
fliod <kgraceaz@gmail.com>
2014-12-18 15:42:27 -0700

    Update CellString.java

--- src/CellString.java
+++ src/CellString.java
@@ -1,1 +1,33 @@
 // placeholder
+public class CellString extends Cell
+{
+  public String toString()
+  {
+    String result=getValue();
+    if(result.length()==0)
+    {
+      result="<empty>";
+    }
+    else
+    {
+      result="\""+result+"\"";
+    }
+    return result;
+  }
+  public CellString()
+  {
+    super.setValue("");
+  }
+  public CellString(String in)
+  {
+    super.setValue(in);
+  }
+  public String value()
+  {
+    return super.getValue();
+  }
+  public void setValue(String in)
+  {
+    super.setValue(in);
+  }
+}
