3406a32fbc1059cb4b6172abbc5cc2c2a4461dfd
fliod <kgraceaz@gmail.com>
2014-12-18 23:17:34 -0700

    Update VisiCalc.java

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -1,4 +1,4 @@
-public class VisiCalc {
+public class VisiCalc{
 
  public static void main(String[] args) {
   // create an array of cells
@@ -13,8 +13,16 @@ public class VisiCalc {
    cells[0]=new CellString();
    cells[1]=new CellString("wow");
    cells[2]=new CellString();
-  
+   cells[3]=new CellString("wow this is a long string");
+   cells[4]=new CellString();
+   cells[5]=new CellString();
+   cells[6]=new CellNumeric(105.12);
+   cells[7]=new CellNumeric(3.14159265359);
+   cells[8]=new CellString();
+   cells[9]=new CellString();
+   cells[10]=new CellDate("01/01/1990");
+   cells[11]=new CellDate("12/25/2014");
   // print the array
-
+   System.out.print(cells.toString());
  }
 }
