3fa3acd3607fa27a42d4bf3b48bbf6aa3f8a9899
fliod <kgraceaz@gmail.com>
2015-01-12 02:10:31 -0700

    Update CellString.java

--- src/CellString.java
+++ src/CellString.java
@@ -1,33 +1,34 @@
-// placeholder
-public class CellString extends Cell
-{
-  public String toString()
-  {
-    String result=getValue();
-    if(result.length()==0)
-    {
-      result="<empty>";
+
+public class CellString extends Cell {
+  
+  public CellString() {
+    super("");
+  }
+  
+  public CellString(String input) {
+    super(input);
+  }
+  
+  public String getInputValue() {
+    String inputString = getInputString();
+    if (inputString.length() == 0) {
+      return "<empty>";
     }
-    else
-    {
-      result="\""+result+"\"";
+    return inputString = "\"" + inputString + "\"";
+  }
+  
+  public String getDisplayString() {
+    String toDisplay = getInputString();
+    
+    //remove start & end quotes from string
+    if (toDisplay.startsWith("\"")) {
+      toDisplay = toDisplay.substring(1);
+      if (toDisplay.endsWith("\"")) {
+        toDisplay = toDisplay.substring(0, toDisplay.length() - 1);
+      }
     }
-    return result;
-  }
-  public CellString()
-  {
-    super.setValue("");
-  }
-  public CellString(String in)
-  {
-    super.setValue(in);
-  }
-  public String value()
-  {
-    return super.getValue();
-  }
-  public void setValue(String in)
-  {
-    super.setValue(in);
+    // escape double quotes in strings
+    toDisplay.replaceAll("\"", "\\\"");
+    return toDisplay;
   }
 }
