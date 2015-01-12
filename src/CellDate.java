362d8feb9bb73dbf051a88c9248c0e85d6998664
fliod <kgraceaz@gmail.com>
2015-01-12 02:10:59 -0700

    Update CellDate.java

--- src/CellDate.java
+++ src/CellDate.java
@@ -4,28 +4,36 @@ import java.util.*;
 public class CellDate extends Cell {
   
   private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-  public CellDate()
-  {
-    super();
-  }
-  public CellDate(String in)
-  {
-    super.setValue(in);
+  
+  public CellDate() {
+    super("");
   }
   
-  public void setValue(String input) 
-  {
+  public CellDate(String input) {
+    super(input);
+  }
+  
+  public String getInputValue() {
     Date date;
-    try 
-    {
-      date = dateFormat.parse(input); 
-      input = dateFormat.format(date);
-    } 
-    catch (ParseException e) 
-    {
-      input = "";
+    String displayString;
+    try {
+      date = dateFormat.parse(getInputString()); 
+      displayString = dateFormat.format(date);
+    } catch (ParseException e) {
+      displayString = "<empty>";
     }
-    super.setValue(input);
+    return displayString;
   }
   
+  public String getDisplayString() {
+    Date date;
+    String displayString;
+    try {
+      date = dateFormat.parse(getInputString()); 
+      displayString = dateFormat.format(date);
+    } catch (ParseException e) {
+      displayString = "";
+    }
+    return displayString;
+  }
 }
